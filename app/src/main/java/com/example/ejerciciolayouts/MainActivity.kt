bb653f0163e2813e13fd794ec854c45e6611ef64
luissancar <luissancar@yahoo.es>
2024-09-19 20:59:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ejerciciolayouts/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.ejerciciolayouts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.ejerciciolayouts.ui.theme.EjercicioLayoutsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EjercicioLayoutsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Layouts()
+}
+
+@Composable
+fun Layouts() {
+    Row(Modifier.fillMaxSize()) {
+        Spacer(Modifier.width(5.dp))
+        Columna(Modifier.weight(1f), Modifier.weight(0.3f))
+        Spacer(Modifier.width(5.dp))
+        Columna(Modifier.weight(0.8f), Modifier.weight(0.1f))
+        Spacer(Modifier.width(5.dp))
+        Columna(Modifier.weight(1f), Modifier.weight(0.4f))
+        Spacer(Modifier.width(5.dp))
+    }
+}
+
+
+@Composable
+fun Columna(ancho: Modifier, alto: Modifier) {
+    Column(
+        ancho
+    ) {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .background(Color.Red)
+                .weight(1f), contentAlignment = Alignment.Center
+
+        ) {
+            Text("View")
+        }
+        Box(
+            alto
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EjercicioLayoutsTheme {
+        Greeting("Android")
+    }
+}
